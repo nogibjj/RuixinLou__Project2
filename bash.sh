987d3c450f50c43225230ce369f8f7191d9865e1
RuixinLou <70648104+RuixinLou@users.noreply.github.com>
2022-10-02 02:41:36 +0000

    second

--- bash.sh
+++ bash.sh
@@ -6,16 +6,19 @@ read name
 echo "It's nice to meet you $name"
 
 #filtering function
-echo "enter the file name"
+echo "Enter the file name"
 read file
-comp1=$18
-comp2=$60
-if [ -e "$file" ]; then
-    while read -r line
-    if [ "$line" -gt "$comp1" ] && [ "$line" -lt "$comp2"]
-    then 
+if [[ -e "$file" ]]
+then
+    #while read line; do echo $line; done < $file
+    read line
+    if [ "$line" -gt 18 ] && [ "$line" -lt 60]
+    then
         echo "Suitable to work"
         "$line" -o filtered.txt
     else
-        echo "Age is not suitable to work"
+        #cho "Age is not suitable to work"
+    fi
+else
+    echo "$file does not exist"
 fi
